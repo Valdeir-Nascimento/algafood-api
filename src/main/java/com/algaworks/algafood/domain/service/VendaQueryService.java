78288b1daec0ad4903bd292482a80ff87475b7a7
Valdeir Nascimento <valdeircn11@gmail.com>
2021-10-21 22:49:51 -0300

    Preenchendo um relatório JasperReports com JavaBeans e gerando bytes do PDF

--- src/main/java/com/algaworks/algafood/domain/service/VendaQueryService.java
+++ src/main/java/com/algaworks/algafood/domain/service/VendaQueryService.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Service
 public interface VendaQueryService {
-    List<VendaDiariaDTO> consultarVendasDiarias(VendaDiariaFilter filtro);
+    List<VendaDiariaDTO> consultarVendasDiarias(VendaDiariaFilter filtro, String timeOffset);
 }
--- src/main/java/com/algaworks/algafood/infrastructure/service/query/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/query/VendaQueryServiceImpl.java
@@ -1,4 +1,4 @@
-package com.algaworks.algafood.infrastructure.service;
+package com.algaworks.algafood.infrastructure.service.query;
 
 import com.algaworks.algafood.api.dto.VendaDiariaDTO;
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
@@ -21,14 +21,16 @@ public class VendaQueryServiceImpl implements VendaQueryService {
     private EntityManager manager;
 
     @Override
-    public List<VendaDiariaDTO> consultarVendasDiarias(VendaDiariaFilter filtro) {
+    public List<VendaDiariaDTO> consultarVendasDiarias(VendaDiariaFilter filtro, String timeOffset) {
         var builder = manager.getCriteriaBuilder();
         var query = builder.createQuery(VendaDiariaDTO.class);
         var root = query.from(Pedido.class);
         var predicates = new ArrayList<Predicate>();
+        var functionConvertTzDataCriacao = builder.function("convert_tz",
+                Date.class, root.get("dataCriacao"), builder.literal("+00:00"), builder.literal(timeOffset));
 
         var functionDateDataCriacao = builder.function(
-                "date", Date.class, root.get("dataCriacao"));
+                "date", Date.class, functionConvertTzDataCriacao);
 
         var selection = builder.construct(VendaDiariaDTO.class,
                 functionDateDataCriacao,
