f7ac2c5e16303816143d98db4a146b925d278fc3
Valdeir Nascimento <valdeircn11@gmail.com>
2021-11-16 16:51:16 -0300

    Descrevendo documentação de endpoints de associação de grupos com usuários

--- src/main/java/com/algaworks/algafood/api/controller/UsuarioGrupoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/UsuarioGrupoController.java
@@ -1,18 +1,20 @@
 package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.api.assembler.GrupoDTOAssembler;
+import com.algaworks.algafood.api.controller.swagger.UsuarioGrupoControllerSwagger;
 import com.algaworks.algafood.api.dto.GrupoDTO;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.service.CadastroUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/usuarios/{usuarioId}/grupos")
-public class UsuarioGrupoController {
+@RequestMapping(value = "/usuarios/{usuarioId}/grupos",  produces = MediaType.APPLICATION_JSON_VALUE)
+public class UsuarioGrupoController implements UsuarioGrupoControllerSwagger {
     @Autowired
     private CadastroUsuarioService usuarioService;
     @Autowired
---
+++ src/main/java/com/algaworks/algafood/api/controller/swagger/UsuarioGrupoControllerSwagger.java
@@ -0,0 +1,42 @@
+package com.algaworks.algafood.api.controller.swagger;
+
+import com.algaworks.algafood.api.dto.GrupoDTO;
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import io.swagger.annotations.*;
+
+import java.util.List;
+
+@Api(tags = "Usuários")
+public interface UsuarioGrupoControllerSwagger {
+
+
+    @ApiOperation("Lista os grupos associados a um usuário")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Usuário não encontrado", response = Problem.class)
+    })
+    List<GrupoDTO> listar(@ApiParam(value = "ID do usuário", example = "1", required = true) Long usuarioId);
+
+
+    @ApiOperation("Desassociação de grupo com usuário")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Desassociação realizada com sucesso"),
+            @ApiResponse(code = 404, message = "Usuário ou grupo não encontrado",
+                    response = Problem.class)
+    })
+    void desvincular(
+            @ApiParam(value = "ID do usuário", example = "1", required = true) Long usuarioId,
+            @ApiParam(value = "ID do grupo", example = "1", required = true) Long grupoId);
+
+
+    @ApiOperation("Associação de grupo com usuário")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Associação realizada com sucesso"),
+            @ApiResponse(code = 404, message = "Usuário ou grupo não encontrado",
+                    response = Problem.class)
+    })
+    void vincular(
+            @ApiParam(value = "ID do usuário", example = "1", required = true) Long usuarioId,
+            @ApiParam(value = "ID do grupo", example = "1", required = true) Long grupoId);
+
+
+}
