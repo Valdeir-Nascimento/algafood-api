206c590dee6c650f9ca3a2ad013706f0b0ef7d9b
Valdeir Nascimento <valdeircn11@gmail.com>
2021-11-16 11:40:35 -0300

    Descrevendo documentação de endpoints de fluxo de pedidos

--- src/main/java/com/algaworks/algafood/api/controller/FluxoPedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FluxoPedidoController.java
@@ -1,13 +1,15 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.api.controller.swagger.FluxoPedidoControllerSwagger;
 import com.algaworks.algafood.domain.service.FluxoPedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/pedidos/{codigoPedido}")
-public class FluxoPedidoController {
+@RequestMapping(value = "/pedidos/{codigoPedido}", produces = MediaType.APPLICATION_JSON_VALUE)
+public class FluxoPedidoController implements FluxoPedidoControllerSwagger {
 
     @Autowired
     private FluxoPedidoService fluxoPedidoService;
