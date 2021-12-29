818e8e8def560634283002da744dd2cda4996b12
Valdeir Nascimento <valdeircn11@gmail.com>
2021-12-29 20:12:00 -0300

    Implementação do RootEntryPoint da API

---
+++ src/main/java/com/algaworks/algafood/api/dto/RootEntryPointDTO.java
@@ -0,0 +1,7 @@
+package com.algaworks.algafood.api.dto;
+
+import org.springframework.hateoas.RepresentationModel;
+
+
+public class RootEntryPointDTO extends RepresentationModel<RootEntryPointDTO> {
+}
