ba010a9330a16d771109931bfa0d42da4db01565
Valdeir Nascimento <valdeircn11@gmail.com>
2021-12-30 21:03:34 -0300

    Corrigindo a documentação dos endpoints de restaurantes e usuários

--- src/main/java/com/algaworks/algafood/api/dto/swagger/RestaurantesEmbeddedDTOSwagger.java
+++ src/main/java/com/algaworks/algafood/api/dto/swagger/RestaurantesEmbeddedDTOSwagger.java
@@ -2,9 +2,13 @@ package com.algaworks.algafood.api.dto.swagger;
 
 import com.algaworks.algafood.api.dto.RestauranteBasicoDTO;
 import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Setter
+@Getter
 @ApiModel("RestaurantesEmbeddedDTO")
 public class RestaurantesEmbeddedDTOSwagger {
 
