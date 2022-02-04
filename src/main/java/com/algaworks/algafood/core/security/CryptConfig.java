4ae1914f09de57a0d25582fbdfbd40b6313bf558
Valdeir Nascimento <valdeircn11@gmail.com>
2022-02-04 14:41:05 -0300

    Refatorando serviços de usuários para usar BCrypt

--- src/main/java/com/algaworks/algafood/core/security/CryptConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/CryptConfig.java
@@ -1,4 +1,4 @@
-package com.algaworks.algafood.core.config;
+package com.algaworks.algafood.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -2,9 +2,11 @@ package com.algaworks.algafood.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
@@ -17,12 +19,26 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .anyRequest().authenticated()
-            .and()
-            	.cors().and()
-                .oauth2ResourceServer().jwt();
+//        http
+//            .authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//            .and()
+//                .csrf().disable()
+//            	.cors().and()
+//                .oauth2ResourceServer().jwt();
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/v1/usuarios")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf()
+                .disable();
     }
 
 }
